365c7e91b313ec858d6c782d49dedc90b61e8507
ilse31 <ilhamprazsetyo33@gmail.com>
2021-09-28 23:52:42 +0700

    create log terminal

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	_handlerUser "bansosman/app/controllers/users"
+	logger "bansosman/app/middleware"
 	_routes "bansosman/app/routes"
 	_servBooks "bansosman/bussiness/users"
 	_repoUsers "bansosman/drivers/mysql/users"
+	"fmt"
+	"log"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/mysql"
@@ -35,7 +35,7 @@ func InitDB(status string) *gorm.DB {
 func main() {
 	db := InitDB("")
 	e := echo.New()
-
+	logger.LogMiddlewareInit(e)
 	// factory of domain
 	usersRepo := _repoUsers.NewRepoMysql(db)
 	usersServe := _servBooks.NewServe(usersRepo, nil)
