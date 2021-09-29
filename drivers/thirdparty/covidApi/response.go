b6fe8f0689148327e6324760e4f5f58d44a746ef
ilse31 <ilhamprazsetyo33@gmail.com>
2021-09-30 00:23:56 +0700

    new file

---
+++ drivers/thirdparty/covidApi/response.go
@@ -0,0 +1,18 @@
+package covidApi
+
+type Response struct {
+	Attributes struct {
+		Fid       int    `json:"FID"`
+		KodeProvi int    `json:"Kode_Provi"`
+		Provinsi  string `json:"Provinsi"`
+		KasusPosi int    `json:"Kasus_Posi"`
+		KasusSemb int    `json:"Kasus_Semb"`
+		KasusMeni int    `json:"Kasus_Meni"`
+	} `json:"attributes"`
+}
+
+// func (resp *Response) toDomain() covid.Domain {
+// 	return covid.Domain{
+// 		Attributes.FID: resp.Attributes.Fid,
+// 	}
+// }
