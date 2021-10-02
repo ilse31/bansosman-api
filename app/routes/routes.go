b4039b0028a70a5a671e1cccf25eaff1b5c88e82
ilse31 <ilhamprazsetyo33@gmail.com>
2021-10-02 23:33:04 +0700

    delete admin login

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,13 +1,8 @@
 package routes
 
 import (
-	"bansosman/app/controllers"
-	"bansosman/app/controllers/admin"
 	"bansosman/app/controllers/apbn"
 	"bansosman/app/controllers/users"
-	m "bansosman/app/middleware"
-	"bansosman/bussiness"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,7 +10,6 @@ import (
 
 type HandlerRoute struct {
 	UsersHandler  users.ControllerUsers
-	AdminHandler  admin.AdminController
 	Apbnhandler   apbn.ApbnController
 	JwtMiddleware middleware.JWTConfig
 }
@@ -29,10 +23,6 @@ func (handler *HandlerRoute) RouteRegister(e *echo.Echo) {
 	user := e.Group("users")
 	user.POST("/register", handler.UsersHandler.Create)
 	user.GET("/login", handler.UsersHandler.Login)
-	//
-	admins := e.Group("admin")
-	admins.POST("/reg", handler.AdminHandler.Register)
-	admins.POST("/login", handler.AdminHandler.Login)
 
 	//auth val
 	users := e.Group("user", middleware.JWTWithConfig(handler.JwtMiddleware))
@@ -45,18 +35,18 @@ func (handler *HandlerRoute) RouteRegister(e *echo.Echo) {
 
 }
 
-func RoleValidation(roleID int, adminController admin.AdminController) echo.MiddlewareFunc {
-	return func(hf echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			claims := m.GetUser(c)
+// func RoleValidation(roleID int, adminController admin.AdminController) echo.MiddlewareFunc {
+// 	return func(hf echo.HandlerFunc) echo.HandlerFunc {
+// 		return func(c echo.Context) error {
+// 			claims := m.GetUser(c)
 
-			userRole := adminController.GetRoleByID(claims.ID)
+// 			userRole := adminController.GetRoleByID(claims.ID)
 
-			if userRole == roleID {
-				return hf(c)
-			} else {
-				return controllers.NewErrorResponse(c, http.StatusForbidden, bussiness.ErrUnauthorized)
-			}
-		}
-	}
-}
+// 			if userRole == roleID {
+// 				return hf(c)
+// 			} else {
+// 				return controllers.NewErrorResponse(c, http.StatusForbidden, bussiness.ErrUnauthorized)
+// 			}
+// 		}
+// 	}
+// }
